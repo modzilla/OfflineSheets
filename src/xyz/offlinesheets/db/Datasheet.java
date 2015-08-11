b8f48c9d46895daf6977cb2475946c4eb436b370
Marcel Nageler <marcel@nageler.org>
2015-08-11 20:39:17 +0200

    add couchdb tests

---
+++ src/xyz/offlinesheets/db/Datasheet.java
@@ -0,0 +1,19 @@
+package xyz.offlinesheets.db;
+
+import java.io.File;
+import java.util.List;
+
+public class Datasheet {
+//	String name;
+	List<String> tags;
+	File pdf;
+	String sourceURL;
+	
+	public Datasheet(String name, List<String> tags, File pdf, String sourceURL) {
+		super();
+//		this.name = name;
+		this.tags = tags;
+		this.pdf = pdf;
+		this.sourceURL = sourceURL;
+	}
+}
