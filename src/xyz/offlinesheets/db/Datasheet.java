8046c3713fd55edbb0217bafe476d5698bd7afe8
Marcel Nageler <marcel@nageler.org>
2015-08-11 21:03:15 +0200

    add getters to Datasheet.java

--- src/xyz/offlinesheets/db/Datasheet.java
+++ src/xyz/offlinesheets/db/Datasheet.java
@@ -16,4 +16,20 @@ public class Datasheet {
 		this.pdf = pdf;
 		this.sourceURL = sourceURL;
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public List<String> getTags() {
+		return tags;
+	}
+
+	public File getPdf() {
+		return pdf;
+	}
+
+	public String getSourceURL() {
+		return sourceURL;
+	}
 }
